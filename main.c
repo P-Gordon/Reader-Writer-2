5fae9319eaa1ee337310d26d36d94e9329d0dfaa
Pete Gordon <petejgordon@outlook.com>
2022-04-25 22:36:54 -0500

    Project creation will add/rename files in next few commits

---
+++ main.c
@@ -0,0 +1,36 @@
+/*
+*Pete Gordon
+*04/21/2021
+*Second Reader Writers Problem
+*/
+
+/*Main
+*Takes in two command line args 1. # of readers; 2. # of writers
+*/
+
+//Includes
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <semaphore.h>
+#include <pthreads.h>
+
+struct readers{
+    pid_t 
+}
+
+
+void int main (char * argv[],int argc) {
+
+    if (argc != 2) {
+        printf("Usage: ./main #READERS #WRITERS");
+        exit(EXIT_FAILURE);
+    }
+
+
+    
+
+    
+    return 0;
+}
